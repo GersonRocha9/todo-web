a11a3ff99003ddae57951fd1c178ca85bdb1a7d3
Gerson Rocha <gersonrocha9@gmail.com>
2022-01-06 16:44:23 -0300

    Atualizando tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import { AddArea } from "./components/AddArea";
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
-    { id: 1, name: "Comprar o pão na padaria", done: false },
-    { id: 2, name: "Comprar um bolo na padaria", done: true },
+    { id: 1, name: "Estudar TypeScript", done: false },
+    { id: 2, name: "Estudar ReactJS", done: false },
   ]);
 
   const handleAddTask = (taskName: string) => {
